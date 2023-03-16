a416d1c8a7bcc9f79c870bec638d15cbb78bfd7a
samwelnyandoro <samwelnyandoro01@gmail.com>
2023-03-15 22:41:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
