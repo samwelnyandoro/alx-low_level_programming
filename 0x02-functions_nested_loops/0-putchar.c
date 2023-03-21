cb8b72973a09a4c715ef698b22f6ea411387776f
samwelnyandoro <samwelnyandoro01@gmail.com>
2023-03-21 02:43:17 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints _putchar followed by a new line
+ * main -Entry point
  * Return: Always 0
  */
 
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - function that prints alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
